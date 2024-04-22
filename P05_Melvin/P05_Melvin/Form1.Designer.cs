a1aca5d53c404af370cc82a0c4895ace97c10ea7
Mel Jones <121836871+MelJ03@users.noreply.github.com>
2024-04-22 17:21:37 -0500

    Add files via upload

---
+++ P05_Melvin/P05_Melvin/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace P05_Melvin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBorrowed = new System.Windows.Forms.TextBox();
+            this.txtMonthsRepay = new System.Windows.Forms.TextBox();
+            this.txtMonthlyPayment = new System.Windows.Forms.TextBox();
+            this.txtInterest = new System.Windows.Forms.TextBox();
+            this.txtPaid = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(192, 220);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Amount Borrowed";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 363);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Total Paid";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(468, 220);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Yearly Interest Rate";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(725, 220);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Monthly Payment";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(725, 363);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Months to Repay";
+            // 
+            // txtBorrowed
+            // 
+            this.txtBorrowed.Location = new System.Drawing.Point(196, 258);
+            this.txtBorrowed.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBorrowed.Name = "txtBorrowed";
+            this.txtBorrowed.Size = new System.Drawing.Size(132, 22);
+            this.txtBorrowed.TabIndex = 6;
+            // 
+            // txtMonthsRepay
+            // 
+            this.txtMonthsRepay.Location = new System.Drawing.Point(729, 405);
+            this.txtMonthsRepay.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMonthsRepay.Name = "txtMonthsRepay";
+            this.txtMonthsRepay.Size = new System.Drawing.Size(132, 22);
+            this.txtMonthsRepay.TabIndex = 7;
+            // 
+            // txtMonthlyPayment
+            // 
+            this.txtMonthlyPayment.Location = new System.Drawing.Point(729, 258);
+            this.txtMonthlyPayment.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMonthlyPayment.Name = "txtMonthlyPayment";
+            this.txtMonthlyPayment.Size = new System.Drawing.Size(132, 22);
+            this.txtMonthlyPayment.TabIndex = 8;
+            // 
+            // txtInterest
+            // 
+            this.txtInterest.Location = new System.Drawing.Point(472, 258);
+            this.txtInterest.Margin = new System.Windows.Forms.Padding(4);
+            this.txtInterest.Name = "txtInterest";
+            this.txtInterest.Size = new System.Drawing.Size(132, 22);
+            this.txtInterest.TabIndex = 9;
+            // 
+            // txtPaid
+            // 
+            this.txtPaid.Location = new System.Drawing.Point(196, 405);
+            this.txtPaid.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPaid.Name = "txtPaid";
+            this.txtPaid.Size = new System.Drawing.Size(132, 22);
+            this.txtPaid.TabIndex = 10;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(492, 399);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 28);
+            this.btnCalculate.TabIndex = 11;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Yu Gothic Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(367, 11);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(301, 31);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Student Loan Calculator";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtPaid);
+            this.Controls.Add(this.txtInterest);
+            this.Controls.Add(this.txtMonthlyPayment);
+            this.Controls.Add(this.txtMonthsRepay);
+            this.Controls.Add(this.txtBorrowed);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Estimate Loan  Payoff";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBorrowed;
+        private System.Windows.Forms.TextBox txtMonthsRepay;
+        private System.Windows.Forms.TextBox txtMonthlyPayment;
+        private System.Windows.Forms.TextBox txtInterest;
+        private System.Windows.Forms.TextBox txtPaid;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ P05_Melvin/P05_Melvin/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace P05_Melvin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //METHods
+        private bool ValidateInput(string Input)
+        {
+            if (string.IsNullOrEmpty(Input))
+            {
+                MessageBox.Show("Enter a value", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (!string.IsNullOrEmpty(Input))
+            {
+                //Convert to decimal
+                if (double.TryParse(Input, out double doubleValue))
+                {
+                    if (doubleValue >= 0) //positive val
+                    {
+                        return true; //valid
+                    }
+                }
+            }
+
+            MessageBox.Show("Enter a valid value", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return false;
+        }
+        private bool ValidateAllData()
+        {
+            if (!ValidateInput(txtBorrowed.Text))
+            {
+                return false;
+            }
+            if (!ValidateInput(txtInterest.Text))
+            {
+                return false;
+            }
+            if (!ValidateInput(txtMonthlyPayment.Text))
+            {
+                return false;
+            }
+            if (!ValidateInput(txtMonthsRepay.Text))
+            {
+                return false;
+            }
+            if (!ValidateInput(txtPaid.Text))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private double ReturnValues()
+        {
+            double Borrowed;
+            double Interest;
+            double monthlyPayment;
+            double monthsRepay;
+            double Paid;
+
+            Borrowed = double.Parse(txtBorrowed.Text);
+            Interest = double.Parse(txtInterest.Text);
+            monthlyPayment = double.Parse(txtMonthlyPayment.Text);
+            monthsRepay = double.Parse(txtMonthsRepay.Text);
+            Paid = double.Parse(txtPaid.Text);
+        }
+
+        //BTN
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ValidateAllData();
+
+
+                // textBoxResults.Text = $"Total Amount Paid: {totalAmountPaid:C}\r\nNumber of Months Left: {numberOfMonthsLeft}";
+
+            }
+            catch 
+            {
+                MessageBox.Show("Invlauid data entered");
+            }
+        }
+
+    }
+}
